e977306ace89a23cf886e26ebebcc22627644ff7
Ayush Verma <78358911+Ayush76-crypto@users.noreply.github.com>
2021-11-05 15:19:23 +0530

    Login Page with firebase auth complted

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import store from "./redux-app/store";
+import { Provider } from "redux-provider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
